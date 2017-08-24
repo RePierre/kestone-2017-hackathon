4e1afbf5f8b2258c33fccd919431fbf043660579
RePierre <petru.rebeja@gmail.com>
2017-08-24 14:43:06 +0300

    Draft implementation of indexing and search.

---
+++ src/tfidfanalysis.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import glob
+import os
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+glob_pattern = '../data/metadata*/**/*.txt'
+
+files = list(glob.iglob(glob_pattern))
+count_vectorizer = CountVectorizer(input='filename')
+count_vect = count_vectorizer.fit_transform(files)
+
+idx_radium = count_vectorizer.vocabulary_['radium']
+print("Trained counts shape: ")
+print(count_vect.shape)
+print("Index of 'radium': %s" % (idx_radium))
+
+tf_transformer = TfidfTransformer(use_idf=True)
+train_tfidf = tf_transformer.fit_transform(count_vect)
+train_tfidf.shape
+
+radium_related = train_tfidf[:, idx_radium]
+[(file, score) for file, score in zip(files, radium_related)]
+
+
+def search(term):
+    term_index = count_vectorizer.vocabulary_[term]
+    frequency_matrix_slice = train_tfidf[:, term_index]
+    filenames = [os.path.split(file)[1] for file in files]
+    documents = [(filenames[i], frequency_matrix_slice[i, 0])
+                 for i in range(len(files))
+                 if frequency_matrix_slice[i, 0] > 0]
+    ordered = sorted(documents, key=lambda doc: doc[1], reverse=True)
+    return ordered
+
+
+def print_results(results):
+    for t in results:
+        print('%s\t%f' % t)
